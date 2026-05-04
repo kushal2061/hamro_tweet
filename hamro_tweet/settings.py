e0b6c81903ad3d5a375dcbd6283ac63b6ba834ef
Kushal Kandel <136981558+kushal2061@users.noreply.github.com>
2026-05-04 16:56:17 +0545

    Update settings.py

--- hamro_tweet/settings.py
+++ hamro_tweet/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     'tweet',
 ]
 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
