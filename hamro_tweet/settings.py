7723f1d52ccc74c09a6e02c326c52b2b3e75689a
Kushal Kandel <136981558+kushal2061@users.noreply.github.com>
2026-05-04 17:00:12 +0545

    Update settings.py

--- hamro_tweet/settings.py
+++ hamro_tweet/settings.py
@@ -132,8 +132,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL='/accounts/login'
 
