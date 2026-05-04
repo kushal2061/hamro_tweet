d210bd9c99c999dcf70ab7b0aa593b4c6411e603
Kushal Kandel <136981558+kushal2061@users.noreply.github.com>
2026-05-04 15:15:35 +0545

    Restrict ALLOWED_HOSTS for production settings

--- hamro_tweet/settings.py
+++ hamro_tweet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q24^4shzkzau5nz0bxq!eb!zta()bl7(sc=$v&_9(xfe+@=f2_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['hamro-tweet.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
