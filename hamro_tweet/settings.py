18bca135ff73d8f6f38cf2404c2e3a1c8d0cf0ee
Kushal Kandel <136981558+kushal2061@users.noreply.github.com>
2026-05-04 16:32:39 +0545

    Update settings.py

--- hamro_tweet/settings.py
+++ hamro_tweet/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import cloudinary
+import cloudinary.api
+import cloudinary.uploader
+import os
+
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
