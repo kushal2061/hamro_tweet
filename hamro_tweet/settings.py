a703d04f0863982f536e0b65589269144df76332
Kushal Kandel <136981558+kushal2061@users.noreply.github.com>
2026-05-04 16:36:25 +0545

    Update settings.py

--- hamro_tweet/settings.py
+++ hamro_tweet/settings.py
@@ -20,7 +20,8 @@ import os
 cloudinary.config(
     cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
     api_key=os.getenv('CLOUDINARY_API_KEY'),
-    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
+    secure=True
 )
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
